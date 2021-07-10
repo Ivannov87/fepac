0dc9311798157e6d5a6a72b2a4dfe5fe1a78d81c
Ivan Renteria <30631018+Ivannov87@users.noreply.github.com>
2021-07-10 00:26:07 -0500

    first commit
    update site

---
+++ assets/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "falta agregar un nombre !",
+                    minlength: "el nombre debe contener almenos 2 caracteres"
+                },
+                subject: {
+                    required: "falta agregar un motivo del correo !",
+                    minlength: "el motivo del correo debe tener almenos 4 caracteres"
+                },
+                number: {
+                    required: "falta agregar un número !",
+                    minlength: "el número debe contener almenos 5 caracteres"
+                },
+                email: {
+                    required: "sin correo, sin mensaje"
+                },
+                message: {
+                    required: "tienes que escribir algo para enviar este formulario !",
+                    minlength: "debes escribir algo más, el mensaje es muy corto ... "
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
