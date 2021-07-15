b64532ce1d1ec5aaa18228a989c1b4ce3569dbc1
Ivan Renteria <30631018+Ivannov87@users.noreply.github.com>
2021-07-14 22:37:04 -0500

    add counter
    visits

---
+++ contador.php
@@ -0,0 +1,30 @@
+<?php
+
+//function counter()
+//{
+    $archivo = "counter.txt";
+    $f = fopen($archivo,"r");
+    $contador =0;
+
+    if($f)
+    {
+        $contador = fread($f,filesize($archivo));
+        $contador = $contador+1;
+        fclose($f);
+    }
+
+    $f = fopen($archivo,"w+");
+    if($f)
+    {
+        
+        fwrite($f,$contador);
+        fclose($f);
+    }
+
+    //return $contador;
+//}
+
+
+echo  $contador;
+
+?>
